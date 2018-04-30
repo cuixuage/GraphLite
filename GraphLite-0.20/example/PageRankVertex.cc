19580043bec2aa3391865c3cd7ffe0a5c3a19a04
cuixuange <1157097502@qq.com>
2018-04-30 15:00:02 +0800

    add some comment

--- GraphLite-0.20/example/PageRankVertex.cc
+++ GraphLite-0.20/example/PageRankVertex.cc
@@ -67,11 +67,11 @@ public:
         unsigned long long from;
         unsigned long long to;
         double weight = 0;
-
+        
         double value = 1;
         int outdegree = 0;
-
-        const char *line= getEdgeLine();        //FIXME: offset作用
+        
+        const char *line= getEdgeLine();
 
         // Note: modify this if an edge weight is to be read
         //       modify the 'weight' variable
@@ -109,9 +109,7 @@ public:
         char s[1024];
 
         for (ResultIterator r_iter; ! r_iter.done(); r_iter.next() ) {
-            //// 引用、指针作为实参地址传递的区别
             r_iter.getIdValue(vid, &value);
-            //n=写入buffer字符数
             int n = sprintf(s, "%lld: %f\n", (unsigned long long)vid, value);
             writeNextResLine(s, n);
         }
@@ -122,8 +120,8 @@ public:
 class VERTEX_CLASS_NAME(Aggregator): public Aggregator<double> {
 public:
     void init() {
-        m_global = 0;           //master：aggregator value
-        m_local = 0;            //worker：aggregator value
+        m_global = 0;
+        m_local = 0;
     }
     void* getGlobal() {
         return &m_global;
@@ -155,21 +153,16 @@ public:
                     voteToHalt(); return;
                 }
             }
-            //calculate pagerank in current superste
+
             double sum = 0;
             for ( ; ! pmsgs->done(); pmsgs->next() ) {
                 sum += pmsgs->getValue();
             }
             val = 0.15 + 0.85 * sum;
 
-            //上一次的超步结果的value - 当前val的收敛性判断
             double acc = fabs(getValue() - val);
-            accumulateAggr(0, &acc);                //'0', which is the aggregator ID.
+            accumulateAggr(0, &acc);
         }
-
-        //1.change currentNode vertexValue (return by pointer)
-        //2.send the same messageValue to outedge-target vertex:
-        //                         value = my_pangerank / my_outedge
         * mutableValue() = val;
         const int64_t n = getOutEdgeIterator().size();
         sendMessageToAllNeighbors(val / n);
@@ -201,9 +194,9 @@ public:
         m_pin_path = argv[1];
         m_pout_path = argv[2];
 
-        aggregator = new VERTEX_CLASS_NAME(Aggregator)[1];   //// allocate memory for the Aggregator
+        aggregator = new VERTEX_CLASS_NAME(Aggregator)[1];
         regNumAggr(1);
-        regAggr(0, &aggregator[0]);             ////        regAggr(0, &aggregator[0]);
+        regAggr(0, &aggregator[0]);
     }
 
     void term() {
@@ -228,4 +221,3 @@ extern "C" void destroy_graph(Graph* pobject) {
     delete ( VERTEX_CLASS_NAME(InputFormatter)* )(pobject->m_pin_formatter);
     delete ( VERTEX_CLASS_NAME(Graph)* )pobject;
 }
-
